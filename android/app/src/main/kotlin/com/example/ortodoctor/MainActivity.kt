1fc83a497e74cc6211bbb47c56de9f3c5c9d1fe0
Morais <anaceciliamorais27@gmail.com>
2026-04-09 15:30:46 -0300

    chore: initial project structure and folder organization

---
+++ android/app/src/main/kotlin/com/example/ortodoctor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ortodoctor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_client.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+void main(){
+  runApp(MaterialApp(
+    home:Container(
+      decoration: BoxDecoration(
+        border: Border.all(widt: 5, color: Colors.green)
+      ),
+    )
+  ))
+}
